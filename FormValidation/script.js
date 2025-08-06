5cfe4f7503c9fda53adfc2b4cc1c347aa9da653c
srivatchangv2006 <40001172+srivatchangv2006@users.noreply.github.com>
2025-08-06 10:39:02 +0530

    Update script.js

--- FormValidation/script.js
+++ FormValidation/script.js
@@ -1,6 +1,7 @@
 document.getElementById('registrationForm').addEventListener('submit', function (e) {
     e.preventDefault();
     let valid = true;
+    
     const name = document.getElementById('name');
     const email = document.getElementById('email');
     const phone = document.getElementById('phone');
